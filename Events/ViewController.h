1ab06782b235bf99aeff5163a8a56dd7db74f7ae
user <IOS@Macmini-1.local>
2017-01-31 14:15:41 +0530

    Initial Commit

---
+++ Events/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Events
+//
+//  Created by user on 1/31/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Events/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Events
+//
+//  Created by user on 1/31/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
